501a5246060a2459729626e5a4d7a2199fa2eaa2
SodaCris <18463922396@163.com>
2021-11-08 12:18:28 +0800

    remove django key

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+=j-9rhxoh7etigf$@07w8zq(mekn8!hxbc@_-yg8=_lcmjb%^'
+try:
+    import local_settings
+except:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
